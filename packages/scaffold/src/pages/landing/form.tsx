589c500349acbebf1a79a9c58d7876105b3ca255
chaeyoung103 <thdcodud103@naver.com>
2024-10-13 21:06:47 +0900

    chore: add react hook form

--- packages/scaffold/src/pages/landing/form.tsx
+++ packages/scaffold/src/pages/landing/form.tsx
@@ -1,3 +1,45 @@
-export default function LandingFormPage() {
-  return <>LandingFormPage</>;
+import { Container } from "@/components/common/container/Container";
+import TextField from "@/components/common/textField/TextField";
+import { TextFieldContainer } from "@/components/landing/TextFieldContainer";
+import { FormProvider, RegisterOptions, useForm } from "react-hook-form";
+
+export const StoreFieldType = {
+  STORENAME: "storeName",
+  STORETYPE: "storeType",
+  STOREBESTMENU: "storeBestMenu",
+  STORELOCATION: "storeLocation",
+  STOREPRICE: "storePrice",
+  STORETARGET: "storeTarget",
+  STOREMOOD: "storeMood",
+};
+
+interface StoreDTO {
+  storeName: string;
+  storeType: string;
+  storeLocation: string;
+  storePrice: number;
+  storeTarget: string;
+  storeMood: string;
 }
+
+const StoreConfigOption: RegisterOptions = {
+  required: "필수 입력 항목입니다.",
+};
+
+const LandingFormPage = () => {
+  const methods = useForm<StoreDTO>({ mode: "onChange" });
+  return (
+    <FormProvider {...methods}>
+      <Container>
+        <TextFieldContainer
+          id={StoreFieldType.STORENAME}
+          placeholder="우리가게 이름"
+          rightContent="(은)는,"
+          options={StoreConfigOption}
+        />
+      </Container>
+    </FormProvider>
+  );
+};
+
+export default LandingFormPage;
