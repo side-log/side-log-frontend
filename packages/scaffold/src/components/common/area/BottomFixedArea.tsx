4cc48094e11dabd71f750ae616667161c04ff620
chaeyoung103 <thdcodud103@naver.com>
2024-10-13 21:08:02 +0900

    fix: reactPortal로 변경

--- packages/scaffold/src/components/common/area/BottomFixedArea.tsx
+++ packages/scaffold/src/components/common/area/BottomFixedArea.tsx
@@ -1,5 +1,7 @@
 import { css } from "@emotion/react";
 import useWindowInnerWidth from "@/hooks/useInnerWidthHook";
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
 
 interface BottomFixedAreaProps extends React.HTMLAttributes<HTMLDivElement> {
   hasBottomSpace?: boolean;
@@ -9,21 +11,31 @@ interface BottomFixedAreaProps extends React.HTMLAttributes<HTMLDivElement> {
 export const BottomFixedArea = (props: BottomFixedAreaProps) => {
   const { hasBottomSpace = true, children } = props;
 
+  const [mounted, setMounted] = useState(false);
+
   const windowInnerWidth = useWindowInnerWidth();
   const bottomSpace = hasBottomSpace ? "env(safe-area-inset-bottom, 0)" : "0";
 
-  return (
-    <div
-      css={css`
-        position: fixed;
-        bottom: ${bottomSpace};
-        width: ${windowInnerWidth}px;
-        left: 50%;
-        transform: translateX(-50%);
-      `}
-      {...props}
-    >
-      {children}
-    </div>
-  );
+  useEffect(() => {
+    setMounted(true); // 컴포넌트가 클라이언트에 마운트된 후에만 실행
+    return () => setMounted(false); // 컴포넌트가 언마운트되면 해제
+  }, []);
+
+  return mounted
+    ? createPortal(
+        <div
+          css={css`
+            position: fixed;
+            bottom: ${bottomSpace};
+            width: ${windowInnerWidth}px;
+            left: 50%;
+            transform: translateX(-50%);
+          `}
+          {...props}
+        >
+          {children}
+        </div>,
+        document.body
+      )
+    : null;
 };
