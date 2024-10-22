60847fc04c314cea3530f83faa997866f72e7202
전진호 <jinhoda@toss.im>
2024-10-23 01:25:16 +0900

    feat: add gradient background

--- packages/scaffold/src/components/common/area/BottomFixedArea.tsx
+++ packages/scaffold/src/components/common/area/BottomFixedArea.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
-import React, { PropsWithChildren, useEffect, useState } from 'react';
+import { getOs } from '@yeaaaah/shared';
+import React, { useEffect, useState } from 'react';
 import useIsOnScreenKeyboardOpen from '@/hooks/useIsOnScreenKeyboardOpen';
 import useViewportSize from '@/hooks/useViewportSize';
 
@@ -7,16 +8,16 @@ interface BottomFixedAreaProps extends React.HTMLAttributes<HTMLDivElement> {
   hasBottomSpace?: boolean;
 }
 
-export const BottomFixedArea = ({ children, ...rest }: PropsWithChildren<BottomFixedAreaProps>) => {
+export const BottomFixedArea: React.FC<BottomFixedAreaProps> = ({ children, ...rest }) => {
   const [offset, setOffset] = useState(0);
   const isKeypadOpen = useIsOnScreenKeyboardOpen();
   const viewport = useViewportSize();
 
-  const isMobile = true;
+  const isMobile = getOs() === 'iOS' || getOs() === 'Android';
 
   useEffect(() => {
     const handleResize = () => {
-      if (isMobile && isKeypadOpen) {
+      if (isKeypadOpen && isMobile) {
         const visibleHeight = window.innerHeight;
         setOffset(visibleHeight - (viewport?.height ?? 0));
       }
@@ -28,25 +29,36 @@ export const BottomFixedArea = ({ children, ...rest }: PropsWithChildren<BottomF
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [isMobile, isKeypadOpen, viewport]);
+  }, [isKeypadOpen, isMobile, viewport]);
 
   return (
-    <StyledBottomFixedArea
-      style={isKeypadOpen ? { transform: `translateY(-${offset}px)` } : { transform: `translateY(0)` }}
-      {...rest}
-    >
+    <StyledBottomFixedArea style={{ transform: `translateY(${isKeypadOpen ? `-${offset}px` : '0'})` }} {...rest}>
+      <GradientBackground />
       {children}
     </StyledBottomFixedArea>
   );
 };
+
+const GradientBackground = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  pointer-events: none;
+  height: 34px;
+  background: linear-gradient(to top, #ffffff, rgba(255, 255, 255, 0));
+`;
 
 const StyledBottomFixedArea = styled.div`
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
-  padding: 16px;
   z-index: 1000;
   transition: transform 0.3s ease;
-  padding-bottom: env(safe-area-inset-bottom);
+  padding: 32px 20px;
+  overflow: hidden;
+  padding-bottom: calc(max(env(safe-area-inset-bottom), 16px));
 `;
