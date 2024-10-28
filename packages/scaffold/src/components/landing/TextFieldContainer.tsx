63cfdc49b312b6b778934afe1cfc55c3240a713e
전진호 <jinhoda@toss.im>
2024-10-29 01:52:29 +0900

    feat: calculate maxLength for input

--- packages/scaffold/src/components/landing/TextFieldContainer.tsx
+++ packages/scaffold/src/components/landing/TextFieldContainer.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { useEffect, useRef, useState } from 'react';
 import { RegisterOptions } from 'react-hook-form';
 import { Row } from '../common/flex/Flex';
 import Txt from '../common/text/Txt';
@@ -21,9 +22,33 @@ export const TextFieldContainer = ({
 }: TextFieldContainerProps) => {
   const { register } = useLandingFormContext();
 
+  const containerRef = useRef<HTMLDivElement>(null);
+  const emojiRef = useRef<HTMLDivElement>(null);
+  const rightContentRef = useRef<HTMLDivElement>(null);
+  const [maxWidth, setMaxWidth] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    if (containerRef.current && emojiRef.current && rightContentRef.current) {
+      const containerWidth = containerRef.current.offsetWidth;
+      const emojiWidth = emojiRef.current.offsetWidth;
+      const rightContentWidth = rightContentRef.current.offsetWidth;
+
+      const calculatedMaxWidth = containerWidth - emojiWidth - rightContentWidth - 20;
+      setMaxWidth(calculatedMaxWidth);
+      console.log(
+        '🚀 ~ useEffect ~ containerWidth:',
+        containerWidth,
+        emojiWidth,
+        rightContentWidth,
+        calculatedMaxWidth
+      );
+    }
+  }, []);
+
   return (
-    <Row gap={12} alignItems="center">
+    <Row gap={12} alignItems="center" ref={containerRef}>
       <div
+        ref={emojiRef}
         css={css`
           display: flex;
           justify-content: center;
@@ -42,13 +67,27 @@ export const TextFieldContainer = ({
         gap={8}
         css={css`
           width: fit-content;
+          align-items: center;
         `}
-        alignItems="center"
       >
-        <TextField {...textFieldAttributes} {...register(name, rules)} />
-        <Txt font="Pretendard" size="1.6rem" height={24} weight="500" color="#28292C">
-          {rightContent}
-        </Txt>
+        <TextField
+          css={css`
+            max-width: ${maxWidth ? `${maxWidth}px` : '100%'};
+            white-space: nowrap;
+          `}
+          {...textFieldAttributes}
+          {...register(name, rules)}
+        />
+        <div
+          ref={rightContentRef}
+          css={css`
+            white-space: nowrap;
+          `}
+        >
+          <Txt font="Pretendard" size="1.6rem" height={24} weight="500" color="#28292C">
+            {rightContent}
+          </Txt>
+        </div>
       </Row>
     </Row>
   );
