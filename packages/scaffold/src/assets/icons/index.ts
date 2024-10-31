4af7a359b2f1b88e0dac7faf6061b78bffae4f5a
chaeyoung103 <thdcodud103@naver.com>
2024-10-31 14:56:26 +0900

    feat: add triangle svg

--- packages/scaffold/src/assets/icons/index.ts
+++ packages/scaffold/src/assets/icons/index.ts
@@ -2,5 +2,6 @@ import ClockIcon from '@/assets/icons/clock.svg';
 import LeftChevronIcon from '@/assets/icons/left-chevron.svg';
 import LogoIcon from '@/assets/icons/logo.svg';
 import StampIcon from '@/assets/icons/stamp.svg';
+import TriangleIcon from '@/assets/icons/triangle.svg';
 
-export { LeftChevronIcon, StampIcon, LogoIcon, ClockIcon };
+export { LeftChevronIcon, StampIcon, LogoIcon, ClockIcon, TriangleIcon };
