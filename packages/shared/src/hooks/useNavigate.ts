22b9bd30884b84ff1f04522ee802fa124afc6e4d
chaeyoung103 <thdcodud103@naver.com>
2024-11-20 12:18:44 +0900

    fix: 서버사이드 환경 고려, navigate 함수 분리

--- packages/shared/src/hooks/useNavigate.ts
+++ packages/shared/src/hooks/useNavigate.ts
@@ -5,18 +5,20 @@ export function useNavigate(url?: string) {
   const router = useRouter();
 
   const queryParams = useMemo(() => {
-    if (url) {
+    if (url && window != null) {
       const urlObj = new URL(url, window.location.origin);
       return Object.fromEntries(urlObj.searchParams.entries());
     }
-    return { ...router.query };
+    return { ...(router.query || {}) };
   }, [router.query, url]);
+
+  const navigate = (pathname: string, query?: Record<string, string | string[] | undefined>) => {
+    router.push({ pathname, query: { ...queryParams, ...query } });
+  };
 
   return {
     ...router,
     queryParams,
-    navigate: (pathname: string, query?: object) => {
-      router.push({ pathname, query: { ...queryParams, ...query } });
-    },
+    navigate,
   };
 }
