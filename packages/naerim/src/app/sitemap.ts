c51ca56512f34a7221a8259805bdd8bb0c169e5d
Jinho1011 <gino9940@gmail.com>
2025-03-15 15:43:57 +0900

    feat: update sitemap

--- packages/naerim/src/app/sitemap.ts
+++ packages/naerim/src/app/sitemap.ts
@@ -3,19 +3,25 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://recodelog.com',
+      url: 'https://naerim.vercel.app/landing',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://recodelog.com/about',
+      url: 'https://naerim.vercel.app/checklist',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://recodelog.com/blog',
+      url: 'https://naerim.vercel.app/equipment',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://naerim.vercel.app/equipment/1',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
